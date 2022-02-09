404bddcbd8ca115e8a65b2aad38cf6d943d6e8d9
Joao Paulo <fariaslimajp@gmail.com>
2022-02-09 20:49:35 -0300

    adicionando função de descriptografar texto

--- js/criptografia.js
+++ js/criptografia.js
@@ -1,28 +1,89 @@
 
 function criptografaTexto(texto){
     var textoCriptografado = "";
-    texto.forEach(function(letra){
-        switch(letra) {
-            case "a":
-                textoCriptografado += "ai";
-                break;
-            case "e":
-                textoCriptografado += "enter";
-                break;
-            case "i":
-                textoCriptografado += "imes";
-                break;
-            case "o":
-                textoCriptografado += "ober";
-                break;
-            case "u":
-                textoCriptografado += "ufat";
-                break;
-            default:
-                textoCriptografado += letra;
+    if (texto.length == 0) {
+        textoCriptografado = "Caixa de Texto vazia.";
+
+    } else {
+        texto.forEach(function(letra){
+            switch(letra) {
+                case "a":
+                    textoCriptografado += "ai";
+                    break;
+
+                case "e":
+                    textoCriptografado += "enter";
+                    break;
+
+                case "i":
+                    textoCriptografado += "imes";
+                    break;
+
+                case "o":
+                    textoCriptografado += "ober";
+                    break;
+
+                case "u":
+                    textoCriptografado += "ufat";
+                    break;
+
+                default:
+                    textoCriptografado += letra;
+            }
+        });
+    }
+    return textoCriptografado;
+}
+
+function descriptografaTexto(texto){
+    var textoDescriptografado = "";
+    if (texto.length == 0) {
+        textoCriptografado = "Caixa de Texto vazia.";
+
+    } else {
+       for (var i = 0; i < texto.length; i++){
+            switch(texto[i]) {
+                case "a":
+                    if (texto[i+1] == "i") {
+                        textoDescriptografado += "a";
+                        i++;
+                        break;
+                    }
+
+
+                case "e":
+                    if (texto[i+1] == "n" && texto[i+2] == "t" && texto[i+3] == "e" && texto[i+4] == "r"){
+                        textoDescriptografado += "e";
+                        i += 4;
+                        break;
+                    }
+                case "i":
+                    if (texto[i+1] == "m" && texto[i+2] == "e" && texto[i+3] == "s"){
+                        textoDescriptografado += "i";
+                        i += 3;
+                        break;
+                    }
+                case "o": //ober
+                if (texto[i+1] == "b" && texto[i+2] == "e" && texto[i+3] == "r"){
+                    textoDescriptografado += "o";
+                    i += 3;
+                    break;
+                }
+                case "u":
+                    if (texto[i+1] == "f" && texto[i+2] == "a" && texto[i+3] == "t"){
+                        textoDescriptografado += "u";
+                        i += 3;
+                        break;
+                    }
+  
+
+                default:
+                    textoDescriptografado += texto[i];
+            }
         }
-    })
-    mostraMensagem(textoCriptografado);
+        if (texto.length == textoDescriptografado.length) textoDescriptografado = "Este texto já é descriptografado";
+    }
+    return textoDescriptografado;
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
 var textoDeEntrada = document.querySelector("#input-texto");
 var btnCriptografar = document.querySelector("#btn-cripto");
+var btnDescriptografar = document.querySelector("#btn-descripto");
+
+//evento do botão criptografar
 btnCriptografar.addEventListener("click", function(event){
-    event.preventDefault();
+    event.preventDefault(); // evita que a página atualize ao clicar no botão do formulário
+
+    var arrayTexto = lerTexto();
+
+    var textoCriptografado = criptografaTexto(arrayTexto);
+
+    mostraMensagem(textoCriptografado);
+});
+
+//evento do botão descriptografar
+btnDescriptografar.addEventListener("click", function(event){
+    event.preventDefault(); // evita que a página atualize ao clicar no botão do formulário
+
+    var arrayTexto = lerTexto();
+    var textoDescriptografado = descriptografaTexto(arrayTexto);
+    mostraMensagem(textoDescriptografado);
+});
+
+function lerTexto(){
+    // Ler o texto dentro do input e retorna esse texto como um Array
+
     var texto = textoDeEntrada.value;
     var arrayTexto = texto.split("");
-    criptografaTexto(arrayTexto);
-    console.log(arrayTexto);    
-});
+
+    return arrayTexto;
+}
